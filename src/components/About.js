b14af4bf23f53bca3fe67e9ef0560b9ddd53c4f7
stolenikolic <stolenikolic01@gmail.com>
2025-07-16 13:20:24 +0200

    Fix: eslint error in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          <a href="https://facebook.com" className="btn">
             read more
           </a>
         </article>
